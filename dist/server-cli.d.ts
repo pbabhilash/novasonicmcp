dcb1abed419b20eacdd07e322df28e6f3c9d2ad5
pbabhilash <pbabhilash@yahoo.com>
2025-11-23 17:22:13 +0530

    removed dist

--- dist/server-cli.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Nova Sonic server启动入口
- * 用于启动 WebSocket 服务器
- */
-import "./server";
--- dist/utils/process.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function runInstallScript(scriptPath: string): Promise<void>;
-export declare function getBinaryName(name: string): Promise<string>;
-export declare function getBinaryPath(name?: string): Promise<string>;
-export declare function isBinaryExists(name: string): Promise<boolean>;
